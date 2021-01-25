2e06e965f4bac94de5bfc2625f7cd08e1bf38a93
Sarah Thiery <sarahvthiery@gmail.com>
2021-01-25 13:43:44 -0500

    Added stable HTML elements

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-const div = document.querySelector('div')
+// STABLE ELEMENTS
+
+const airportsUl = document.querySelector('#airports-ul')
+const amenitiesUl = document.querySelector('#amenities-ul')
+const restaurantsUl = document.querySelector('#restaurants-ul')
+const storesUl = document.querySelector('#stores-ul')
+
